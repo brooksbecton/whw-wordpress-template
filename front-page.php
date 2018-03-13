3d11c10023a5067be4d2e3809d87033d3253c6e0
Brooks Becton <broabect@ut.utm.edu>
2018-03-13 16:29:45 -0500

    init

---
+++ front-page.php
@@ -0,0 +1,15 @@
+<?php
+/** Main Template File */
+    get_header(); 
+?> 
+
+    <h1>Welcome</h1>
+
+    <main class="main_content">
+        <p>Welcome to de home password_get_info</p>
+    </main>
+
+
+<?php 
+    get_footer();
+?> 
